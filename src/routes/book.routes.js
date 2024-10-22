fef24e4f4d4564768eeedbcc88942693cc13bb82
Christian <filetelopezchristian@gmail.com>
2024-10-22 08:55:14 -0600

    new request added

--- src/routes/book.routes.js
+++ src/routes/book.routes.js
@@ -1,9 +1,17 @@
+import verifyToken from '../middlewares/verifyToken.middlewares'
 import * as controller from '../controllers/book.controllers'
+import fileUpload from 'express-fileupload'
 import { Router } from 'express'
 
 const router = Router()
 
 router.get('', controller.getBooks)
 router.get('/:id', controller.getBook)
+router.post(
+	'',
+	verifyToken,
+	fileUpload({ useTempFiles: true, tempFileDir: './tmp' }),
+	controller.createBook,
+)
 
 export default router
