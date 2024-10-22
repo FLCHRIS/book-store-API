64d95147b4e4202366af5455a966653ad417ac5d
Christian <filetelopezchristian@gmail.com>
2024-10-22 08:54:26 -0600

    New controller created

--- src/controllers/book.controllers.js
+++ src/controllers/book.controllers.js
@@ -20,9 +20,40 @@ export const getBooks = async (req, res) => {
 export const getBook = async (req, res) => {
 	const { id } = req.params
 
-	if (Number(id) < 1) return res.status(400).json({ message: 'Invalid id' })
+	if (Number(id) < 1) {
+		return res.status(400).json({ message: 'Invalid id' })
+	}
 
 	const { message, status, data } = await service.getBook(Number(id))
+
+	return res.status(status).json({ message, data })
+}
+
+export const createBook = async (req, res) => {
+	const image = req.files?.image
+	const book = req.body
+
+	if (!image) {
+		return res.status(400).json({ message: 'Image not provided' })
+	}
+
+	if (
+		!book.title ||
+		!book.author ||
+		!book.publisher ||
+		!book.year ||
+		!book.price ||
+		!book.stock ||
+		!book.description ||
+		!book.genreId
+	) {
+		return res.status(400).json({ message: 'Missing required fields' })
+	}
+
+	const { status, message, data } = await service.createBook(
+		book,
+		image.tempFilePath,
+	)
 
 	return res.status(status).json({ message, data })
 }
