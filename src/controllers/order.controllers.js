868aac608c3fbe9dea49a123f9b7e2ae4d79cc86
Christian <filetelopezchristian@gmail.com>
2024-10-25 20:19:52 -0600

    New GET_ORDERS controller created

--- src/controllers/order.controllers.js
+++ src/controllers/order.controllers.js
@@ -1,16 +1,30 @@
 import * as service from '../services/order.services'
 
-export const createOrder = async (req, res) => {
-	const { bookId, quantity, userId } = req.body
+export const getOrders = async (req, res) => {
+	const { status } = req.query
+	const user = req.user
 
-	if (!bookId || !quantity || !userId) {
+	const filters = { userId: Number(user.id) }
+
+	if (status) filters.status = status
+
+	const { message, status: statusCode, data } = await service.getOrders(filters)
+
+	return res.status(statusCode).json({ message, data })
+}
+
+export const createOrder = async (req, res) => {
+	const { bookId, quantity } = req.body
+	const user = req.user
+
+	if (!bookId || !quantity) {
 		return res.status(400).json({ message: 'Missing parameters' })
 	}
 
 	const { status, message, data } = await service.createOrder(
 		Number(bookId),
 		Number(quantity),
-		Number(userId),
+		Number(user.id),
 	)
 
 	res.status(status).json({ message, data })
