683ecda07b83ccb22c02b9bcdca442060d9cb9e5
Christian <filetelopezchristian@gmail.com>
2024-10-26 14:36:58 -0600

    New FAVORITES model added

---
+++ prisma/migrations/20241026203616_favorite_model/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE `Favorite` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `userId` INTEGER NOT NULL,
+    `bookId` INTEGER NOT NULL,
+
+    UNIQUE INDEX `Favorite_userId_bookId_key`(`userId`, `bookId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Favorite` ADD CONSTRAINT `Favorite_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Favorite` ADD CONSTRAINT `Favorite_bookId_fkey` FOREIGN KEY (`bookId`) REFERENCES `Book`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
