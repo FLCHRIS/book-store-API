4f6018d86e316733287b7c40a4b1763b49b4f13c
Christian <filetelopezchristian@gmail.com>
2024-10-28 10:20:39 -0600

    New LOG-IN-ADMIN router created

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,12 @@
 import * as controllers from '../controllers/auth.controllers'
+import verifyToken from '../middlewares/verifyToken.middlewares'
+import verifyAdmin from '../middlewares/verifyAdmin.middlewares'
 import { Router } from 'express'
 
 const router = Router()
 
 router.post('/sign-up', controllers.signUp)
+router.post('/sign-up-admin', verifyToken, verifyAdmin, controllers.signUpAdmin)
 router.post('/log-in', controllers.logIn)
 router.post('/log-out', controllers.logOut)
 
