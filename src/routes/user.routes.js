408578903a1841d716f83cbd52d8587010e93c37
Christian <filetelopezchristian@gmail.com>
2024-11-01 15:42:07 -0600

    New routes added

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,11 +1,13 @@
 import verifyToken from '../middlewares/verifyToken.middlewares'
 import verifyAdmin from '../middlewares/verifyAdmin.middlewares'
-import * as controllers from '../controllers/user.controllers'
+import * as controller from '../controllers/user.controllers'
 import { Router } from 'express'
 
 const router = Router()
 
-router.delete('/:id', verifyToken, verifyAdmin, controllers.deleteUser)
-
+router.get('', verifyToken, verifyAdmin, controller.getUsers)
+router.post('', verifyToken, verifyAdmin, controller.createUser)
+router.get('/:id', verifyToken, verifyAdmin, controller.getUser)
+router.delete('/:id', verifyToken, verifyAdmin, controller.deleteUser)
 
 export default router
