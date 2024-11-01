facda2b22c6ee291d5eb75359489c2c95eee3f32
Christian <filetelopezchristian@gmail.com>
2024-11-01 12:45:34 -0600

    New DELETE_ACCOUNT router added

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -9,5 +9,6 @@ router.post('/sign-up', controllers.signUp)
 router.post('/sign-up-admin', verifyToken, verifyAdmin, controllers.signUpAdmin)
 router.post('/log-in', controllers.logIn)
 router.post('/log-out', controllers.logOut)
+router.delete('/delete-account', verifyToken, controllers.deleteAccount)
 
 export default router
