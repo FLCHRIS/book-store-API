cd4eca0b0a1a14cc4113d1ce11308cf4e2e733e3
Christian <filetelopezchristian@gmail.com>
2024-11-03 12:13:41 -0600

    Fixed bug

--- src/app.js
+++ src/app.js
@@ -18,7 +18,10 @@ const app = express()
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors())
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true,
+}))
 
 seedGenre()
 
