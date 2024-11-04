fe15244c88cc14cb0d75fd0be69cda8dd3b72964
Christian <filetelopezchristian@gmail.com>
2024-11-04 09:37:26 -0600

    New routes added

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -9,6 +9,6 @@ router.post('/log-in', controllers.logIn)
 router.post('/log-out', controllers.logOut)
 router.delete('/delete-account', verifyToken, controllers.deleteAccount)
 router.patch('/update-password', verifyToken, controllers.updatePassword)
-router.get('/validate-session', verifyToken, controllers.validateSession)
+router.get('/refresh-token', verifyToken, controllers.refreshToken)
 
 export default router
